6dcb9f8414008ccde0f833bc8f70e72e17b55c5d
Samuel Tijani <omalitijani@gmail.com>
2022-02-09 12:41:10 +0100

    Add APIRecipeQuery.fromJson

---
+++ lib/network/recipe_model.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'recipe_model.g.dart';
+
+@JsonSerializable()
+class APIRecipeQuery {
+  // TODO: Add APIRecipeQuery.fromJson
+  factory APIRecipeQuery.fromJson(Map<String, dynamic> json) =>
+      _$APIRecipeQueryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$APIRecipeQueryToJson(this);
+}
+
+// TODO: Add @JsonSerializable() class APIHits
+// TODO: Add @JsonSerializable() class APIRecipe
+// TODO: Add @JsonSerializable() class APIIngredients

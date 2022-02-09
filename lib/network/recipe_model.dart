ce8614a3abff553d3083c94cf5adbefa8122f571
Samuel Tijani <omalitijani@gmail.com>
2022-02-09 12:59:32 +0100

    Add @JsonSerializable() class APIHits

--- lib/network/recipe_model.dart
+++ lib/network/recipe_model.dart
@@ -9,8 +9,42 @@ class APIRecipeQuery {
       _$APIRecipeQueryFromJson(json);
 
   Map<String, dynamic> toJson() => _$APIRecipeQueryToJson(this);
+
+  @JsonKey(name: 'q')
+  String query;
+  int from;
+  int to;
+  bool more;
+  int count;
+  List<APIHits> hits;
+
+  APIRecipeQuery({
+    required this.query,
+    required this.from,
+    required this.to,
+    required this.more,
+    required this.count,
+    required this.hits,
+  });
 }
 
 // TODO: Add @JsonSerializable() class APIHits
+// 1
+@JsonSerializable()
+class APIHits {
+  // 2
+  APIRecipe recipe;
+
+  // 3
+  APIHits({
+    required this.recipe,
+  });
+
+  // 4
+  factory APIHits.fromJson(Map<String, dynamic> json) =>
+      _$APIHitsFromJson(json);
+  Map<String, dynamic> toJson() => _$APIHitsToJson(this);
+}
+
 // TODO: Add @JsonSerializable() class APIRecipe
 // TODO: Add @JsonSerializable() class APIIngredients

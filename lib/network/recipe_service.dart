b24d064f9bc516a8925c121234100c23dd5768c7
Samuel Tijani <omalitijani@gmail.com>
2022-02-10 19:40:35 +0100

    Generate the Chopper file

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -3,6 +3,7 @@ import 'package:chopper/chopper.dart';
 import 'recipe_model.dart';
 import 'model_response.dart';
 import 'model_converter.dart';
+part 'recipe_service.chopper.dart';
 
 const String apiKey = '0c177b9cd32f8c3261d0c205f263d398';
 const String apiId = 'ccafb53c';
@@ -22,5 +23,34 @@ abstract class RecipeService extends ChopperService {
       @Query('from') int from,
       @Query('to') int to);
 // TODO: Add create()
+  static RecipeService create() {
+    // 1
+    final client = ChopperClient(
+      // 2
+      baseUrl: apiUrl,
+      // 3
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      // 4
+      converter: ModelConverter(),
+      // 5
+      errorConverter: const JsonConverter(),
+      // 6
+      services: [
+        _$RecipeService(),
+      ],
+    );
+    // 7
+    return _$RecipeService(client);
+  }
 }
+
 // TODO: Add _addQuery()
+Request _addQuery(Request req) {
+  // 1
+  final params = Map<String, dynamic>.from(req.parameters);
+  // 2
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+  // 3
+  return req.copyWith(parameters: params);
+}

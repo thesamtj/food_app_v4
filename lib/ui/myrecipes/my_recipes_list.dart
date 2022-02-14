b70174142a3f729cb84667794b8d609f4cd01654
Samuel Tijani <omalitijani@gmail.com>
2022-02-14 12:38:56 +0100

    Implement the Bookmarks screen

--- lib/ui/myrecipes/my_recipes_list.dart
+++ lib/ui/myrecipes/my_recipes_list.dart
@@ -16,13 +16,6 @@ class _MyRecipesListState extends State<MyRecipesList> {
   // TODO: Update recipes declaration
   List<Recipe> recipes = [];
 
-  // TODO: Remove initState()
-  @override
-  void initState() {
-    super.initState();
-    recipes = <String>[];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -33,61 +26,68 @@ class _MyRecipesListState extends State<MyRecipesList> {
 
   Widget _buildRecipeList(BuildContext context) {
     // TODO: Add Consumer
-    return ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (BuildContext context, int index) {
-          // TODO: Add recipe definition
-          return SizedBox(
-            height: 100,
-            child: Slidable(
-              actionPane: const SlidableDrawerActionPane(),
-              actionExtentRatio: 0.25,
-              child: Card(
-                elevation: 1.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                color: Colors.white,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      leading: CachedNetworkImage(
-                          // TODO: Replace imageUrl hardcoding
-                          imageUrl:
-                              'http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html',
-                          height: 120,
-                          width: 60,
-                          fit: BoxFit.cover),
-                      // TODO: Replace title hardcoding
-                      title: const Text('Chicken Vesuvio'),
+    return Consumer<MemoryRepository>(
+      builder: (context, repository, child) {
+        recipes = repository.findAllRecipes();
+
+        return ListView.builder(
+            itemCount: recipes.length,
+            itemBuilder: (BuildContext context, int index) {
+              // TODO: Add recipe definition
+              final recipe = recipes[index];
+
+              return SizedBox(
+                height: 100,
+                child: Slidable(
+                  actionPane: const SlidableDrawerActionPane(),
+                  actionExtentRatio: 0.25,
+                  child: Card(
+                    elevation: 1.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.white,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: CachedNetworkImage(
+                              // TODO: Replace imageUrl hardcoding
+                              imageUrl: recipe.image ?? '',
+                              height: 120,
+                              width: 60,
+                              fit: BoxFit.cover),
+                          // TODO: Replace title hardcoding
+                          title: Text(recipe.label ?? ''),
+                        ),
+                      ),
                     ),
                   ),
+                  actions: <Widget>[
+                    IconSlideAction(
+                        caption: 'Delete',
+                        color: Colors.transparent,
+                        foregroundColor: Colors.black,
+                        iconWidget: const Icon(Icons.delete, color: Colors.red),
+                        // TODO: Update first onTap
+                        onTap: () => deleteRecipe(repository, recipe)),
+                  ],
+                  secondaryActions: <Widget>[
+                    IconSlideAction(
+                        caption: 'Delete',
+                        color: Colors.transparent,
+                        foregroundColor: Colors.black,
+                        iconWidget: const Icon(Icons.delete, color: Colors.red),
+                        // TODO: Update second onTap
+                        onTap: () => deleteRecipe(repository, recipe)),
+                  ],
                 ),
-              ),
-              actions: <Widget>[
-                IconSlideAction(
-                    caption: 'Delete',
-                    color: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    iconWidget: const Icon(Icons.delete, color: Colors.red),
-                    // TODO: Update first onTap
-                    onTap: () {})
-              ],
-              secondaryActions: <Widget>[
-                IconSlideAction(
-                    caption: 'Delete',
-                    color: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    iconWidget: const Icon(Icons.delete, color: Colors.red),
-                    // TODO: Update second onTap
-                    onTap: () {})
-              ],
-            ),
-          );
-        });
-    // TODO: Add final brace and parenthesis
+              );
+            });
+        // TODO: Add final brace and parenthesis
+      },
+    );
   }
 
   // TODO: Add deleteRecipe() here
